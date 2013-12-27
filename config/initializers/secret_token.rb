43fc714daca3a8f73fe816fe61c7eb768887f00f
henry802 <hab.bonges@gmail.com>
2013-12-27 09:21:40 -0500

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Lotto::Application.config.secret_key_base = 'df5101b2d2657244e6ba4cf85f8e273c2dfa2485818d5483b1f489521bc3a06e3a84feac6abb259300202016de0d22656526651619ead8baeaa1d7909a132367'
